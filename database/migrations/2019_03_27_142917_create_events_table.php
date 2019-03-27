5f71685b70bb877d4dadecddc12f27b181f0b15f
rabujev <jamal.rabujev@gmail.com>
2019-03-27 15:40:19 +0100

    readd envexample

---
+++ database/migrations/2019_03_27_142917_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('event_name');
+            $table->date('event_date');
+            $table->string('event_author');
+            $table->string('event_address');
+            $table->text('event_description');
+            $table->float('event_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
