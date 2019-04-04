17ccdabf53ff231b1bf934df9e511305b4fb76a7
Louis <cantinieauxlouis@gmail.com>
2019-04-04 16:45:21 +0200

    email verification enabled more or less needs tests

--- routes/api.php
+++ routes/api.php
@@ -28,11 +28,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', 'AuthController@register')->name('register');
+
 Route::post('/login', 'AuthController@login')->name('login');
+
 Route::post('/logout', 'AuthController@logout')->name('logout');
 
-Mail::raw('EVENT_COMING', function($message) {
+Route::post('/send', 'EmailController@send')->name('send');
+
+/*Mail::raw('EVENT_COMING', function($message) {
   $message->subject('Hi there!');
   $message->from(config('mail.from.address'), config('app.name'));
   $message->to('cantinieauxlouis@gmail.com');
-});
+});*/
