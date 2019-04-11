80999a6ed15f0470df7cc2c7428b3c8de9b08b3e
ThibautJanssens <janssensthib@gmail.com>
2019-04-11 16:28:40 +0200

    adding seeder

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -1,12 +1,11 @@
 <?php
-
 use Faker\Generator as Faker;
-
 $factory->define(App\Event::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence(),
         'date_event' => $faker->dateTime(),
         'description' => $faker->text(),
-        'reminder' => $faker->dateTime()
+        'reminder' => $faker->dateTime(),
+        'author' =>factory ('App\User')->create()->id
     ];
 });
