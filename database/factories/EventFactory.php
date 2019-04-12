c578ebccf9ad2384144356c9361774d67299b09b
ThibautJanssens <janssensthib@gmail.com>
2019-04-12 10:01:27 +0200

    modifyin table event + faker

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -6,6 +6,9 @@ $factory->define(App\Event::class, function (Faker $faker) {
         'date_event' => $faker->dateTime(),
         'description' => $faker->text(),
         'reminder' => $faker->dateTime(),
+        'street' => $faker->sentence(),
+        'city' => $faker->sentence(),
+        'postal_code' => $faker->sentence(),
         'author' =>factory ('App\User')->create()->id
     ];
 });
--- database/migrations/2019_03_27_150622_create_events_table.php
+++ database/migrations/2019_03_27_150622_create_events_table.php
@@ -18,10 +18,15 @@ class CreateEventsTable extends Migration
             $table->timestamps();
             $table->string('name');
             $table->dateTime('date_event');
+            $table->string('street');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('country')->nullable();
             $table->integer('author');
             $table->text('description')->nullable();
             $table->dateTime('reminder')->nullable();
             $table->text('image_url')->nullable();
+            $table->text('video_url')->nullable();
             $table->foreign('author')->references('id')->on('users');
         });
     }
