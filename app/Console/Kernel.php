0a7512d8c0cb17b41b9bbacfc59bce08feefb6db
ThibautJanssens <janssensthib@gmail.com>
2019-04-23 09:23:03 +0200

    deployement stuff

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,27 +29,27 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-            $participations = DB::table('events')
-                ->join('list_of_participants', 'list_of_participants.event', 'events.id')
-                ->join('users', 'list_of_participants.participant', 'users.id')
-                ->select('users.email', 'users.name AS user', 'events.name as event', 'events.date_event', 'events.id')
-                ->where('list_of_participants.reminder_status', 'false')
-                ->where('events.reminder','<=', 'NOW()')
-                ->get();
+            // $participations = DB::table('events')
+            //     ->join('list_of_participants', 'list_of_participants.event', 'events.id')
+            //     ->join('users', 'list_of_participants.participant', 'users.id')
+            //     ->select('users.email', 'users.name AS user', 'events.name as event', 'events.date_event', 'events.id')
+            //     ->where('list_of_participants.reminder_status', 'false')
+            //     ->where('events.reminder','<=', 'NOW()')
+            //     ->get();
 
-                DB::table('list_of_participants')
-                ->join('events', 'list_of_participants.event', 'events.id')
-                ->select('list_of_participants.*')
-                ->where('events.reminder','<=', 'NOW()')
-                ->update(['list_of_participants.reminder_status' => 'true']);
+            //     DB::table('list_of_participants')
+            //     ->join('events', 'list_of_participants.event', 'events.id')
+            //     ->select('list_of_participants.*')
+            //     ->where('events.reminder','<=', 'NOW()')
+            //     ->update(['list_of_participants.reminder_status' => 'true']);
 
-            foreach ($participations as $reminder) {
-                $mail = new ReminderMail($reminder);
-                Mail::to($reminder->email)
-                //->from('doonut@jepsenbrite.com')
-                //->subject($reminder->event)
-                ->send($mail);
-            }
+            // foreach ($participations as $reminder) {
+            //     $mail = new ReminderMail($reminder);
+            //     Mail::to($reminder->email)
+            //     //->from('doonut@jepsenbrite.com')
+            //     //->subject($reminder->event)
+            //     ->send($mail);
+            // }
     }
 
     /**
